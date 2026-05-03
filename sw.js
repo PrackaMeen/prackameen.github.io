86352b450cf5cbafdf0a86f342b0c1a8729a504a
Stefik, Ondrej <ondrej.stefik@gmail.com>
2026-05-04 00:11:52 +0200

    Road4, Chamber4 + animations details

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const APP_VERSION = "1.1.108";
-const APP_COMMIT_SHORT = "068190d";
+const APP_VERSION = "1.1.109";
+const APP_COMMIT_SHORT = "a921060";
 const CACHE_NAME = `game-mobile-admin-v${APP_VERSION}-${APP_COMMIT_SHORT}`;
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=1.1.108",
-  "./app.js?v=1.1.108",
+  "./styles.css?v=1.1.109",
+  "./app.js?v=1.1.109",
   "./player-preferences.js",
   "./pages/menu/index.html",
   "./pages/menu/styles.css",
